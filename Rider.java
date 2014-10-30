a2c1c7db9a47d029b0c27fd82a9f4e28b022b9f4
unknown <tq4@duke.edu>
2014-10-30 11:12:50 -0400

    Began test program for elevator part 1

--- Rider.java
+++ Rider.java
@@ -3,44 +3,47 @@ import java.util.Scanner;
 
 
 public class Rider implements Runnable {
-	private Scanner s;
-	
-	int riderId;
-	Building building;
-	public Rider(int i) { riderId =i;} 
+	private Scanner myScanner;
+	int myRiderId;
+	Building myBuilding;
+	public Rider(int riderId, File input, Building building) {
+			myRiderId = riderId;
+			myBuilding = building;
+			try {
+				myScanner = new Scanner(input);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	} 
 	@Override 
 	public void run() {
-		
-		try {
-			s = new Scanner(new File("input.txt"));
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
+		int iter = 0;
+		while(iter < 4){//move past the first line which contains four tokens
+			myScanner.hasNextInt();
+			iter++;
 		}
-		int numFloors=s.nextInt();  //probably do not need the info here
-		int numElevators = s.nextInt();
-		int numRiders=s.nextInt();
-		int maxOccupancy=s.nextInt();
-		while(s.hasNextInt()){
-			int rider = s.nextInt();
-			int startingFloor = s.nextInt();
-			int destinationFloor = s.nextInt();
-			if (rider == riderId){
+		while(myScanner.hasNextInt()){
+			int rider = myScanner.nextInt();
+			int startingFloor = myScanner.nextInt();
+			int destinationFloor = myScanner.nextInt();
+			if (rider == myRiderId){
 				if(startingFloor<destinationFloor){
-					Elevator e = building.CallUp(startingFloor);
+					Elevator e = myBuilding.CallUp(startingFloor);
 					e.Enter(rider,startingFloor,2); //need to change this, put parameters floor and direction
 					e.RequestFloor(destinationFloor,rider);
 					e.Exit(rider,destinationFloor); //put parameters
 				}
-				if(startingFloor>destinationFloor){
-					Elevator e = building.CallDown(startingFloor);
+				else if(startingFloor>destinationFloor){
+					Elevator e = myBuilding.CallDown(startingFloor);
 					e.Enter(rider,startingFloor,1);
 					e.RequestFloor(destinationFloor,rider);
 					e.Exit(rider,destinationFloor);
 				}
 			}
 		}
-		s.close();
+		myScanner.close();
 		
 	}
 
--- TestP1.java
+++ TestP1.java
@@ -3,5 +3,21 @@ import java.io.*;
 import java.util.*;
 
 public class TestP1 {
-
+		public static void runTest(String inputFilePath){
+			int numFloors, numElevators, numRiders, maxOccupancy;
+			File input = new File(inputFilePath);
+			try {
+				Scanner mainScanner = new Scanner(input);
+				 numFloors = mainScanner.nextInt();  //probably do not need the info here
+				 numElevators = mainScanner.nextInt();
+				 numRiders = mainScanner.nextInt();
+				 maxOccupancy = mainScanner.nextInt();
+				 mainScanner.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
 }
